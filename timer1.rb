46ff5199890522200dfd9b6913db1cac4cf96000
you-ssk <inukichi.i@gmail.com>
2011-01-17 22:45:19 +0900

    first commit

---
+++ timer1.rb
@@ -0,0 +1,30 @@
+require 'gnomecanvas2'
+
+class TimerWindow
+  def initialize(width, height)
+    init_window(width, height)
+  end
+
+  def init_window(width, height, window_type=nil)
+    window_type ||= Gtk::Window::TOPLEVEL
+    @window = Gtk::Window.new(window_type)
+    @window.set_default_size(width,height)
+    set_window_signal
+    @window.show
+  end
+
+  def set_window_signal
+    @window.signal_connect("destroy") do
+      Gtk.main_quit
+    end
+  end
+
+end
+
+def main
+  Gtk.init
+  timer = TimerWindow.new(400,300)
+  Gtk.main
+end
+
+main
