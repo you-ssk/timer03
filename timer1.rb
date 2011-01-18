2359016e14852cbccf310d974b06f1811d8d2bf0
you-ssk <inukichi.i@gmail.com>
2011-01-19 00:04:09 +0900

    test primitive

--- timer1.rb
+++ timer1.rb
@@ -4,22 +4,40 @@ require 'gtk2'
 class TimerWindow
   def initialize(width, height)
     init_window(width, height)
+    init_gc()
+    init_color()
   end
 
   def init_window(width, height, window_type=nil)
     window_type ||= Gtk::Window::TOPLEVEL
     @window = Gtk::Window.new(window_type)
     @window.set_default_size(width,height)
+    @window.set_app_paintable(true)
+    @window.realize
     set_window_signal
-    @window.show
+    @window.show_all
   end
 
   def set_window_signal
     @window.signal_connect("destroy") do
       Gtk.main_quit
     end
+    @window.signal_connect("expose_event") do
+      width,height = @window.default_size
+      @gc.set_foreground(Gdk::Color.new(65535,0,0))
+      @drawable.draw_rectangle(@gc,true,0,0,width,height)
+      @gc.set_foreground(Gdk::Color.new(0,65535,0))
+      @drawable.draw_line(@gc,0,0,width,height)
+    end
   end
 
+  def init_gc
+    @drawable = @window.window
+    @gc = Gdk::GC.new(@drawable)
+  end
+
+  def init_color
+  end
 end
 
 def main
