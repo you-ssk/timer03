36c9039899110181156180f4fe7b9b51a8f23d49
you-ssk <you@Ubuntu10.(none)>
2011-01-20 19:26:31 +0900

    add text

--- timer1.rb
+++ timer1.rb
@@ -13,8 +13,9 @@ end
 class TimerWindow
   def initialize(width, height)
     init_window(width, height)
-    init_gc()
-    init_color()
+    init_gc
+    init_pixmap
+    init_timer
   end
 
   def init_window(width, height, window_type=nil)
@@ -27,33 +28,52 @@ class TimerWindow
     @window.show_all
   end
 
+  def init_gc
+    @drawable = @window.window
+    @gc = Gdk::GC.new(@drawable)
+  end
+
+  def init_pixmap
+#    @pixmap = Gdk::Pixmap.new(@window,
+  end
+  def init_timer
+    @time = Time.now
+    Gtk::timeout_add(100) do
+      draw
+      draw_text
+      true
+    end
+  end
+
   def set_window_signal
     @window.signal_connect("destroy") do
       Gtk.main_quit
     end
     @window.signal_connect("expose_event") do
       draw
+      draw_text
     end
   end
 
   def draw
-    width,height = @window.default_size
+    width,height = @window.size
     @gc.set_rgb_fg_color(Color["blue"])
     @drawable.draw_rectangle(@gc,true,0,0,width,height)
     @gc.set_rgb_fg_color(Color["#00FF00"])
     @drawable.draw_line(@gc,0,0,width,height)
     @gc.set_rgb_fg_color(Color["#FF0000"])
     @drawable.draw_line(@gc,width,0,0,height)
-
   end
 
-  def init_gc
-    @drawable = @window.window
-    @gc = Gdk::GC.new(@drawable)
+  def draw_text
+    width,height = @window.size
+    layout = Pango::Layout.new(@window.pango_context)
+    layout.width = width*Pango::SCALE
+    layout.set_alignment(Pango::Layout::ALIGN_CENTER)
+    layout.text = Time.now.to_s
+    @drawable.draw_layout(@gc, 0, height/2, layout, Color["red"])
   end
 
-  def init_color
-  end
 end
 
 def main
