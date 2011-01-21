483c749f997b26c56a5cf1fd99f540f85648fc7f
you-ssk <inukichi.i@gmail.com>
2011-01-22 00:44:09 +0900

    trivial

--- timer1.rb
+++ timer1.rb
@@ -1,61 +1,6 @@
 #require 'gnomecanvas2'
 require 'gtk2'
-
-class Color
-  @@color_table = Hash.new do |h,k|
-    h[k] = Gdk::Color.parse(k)
-  end
-  def self.[](key)
-    @@color_table[key]
-  end
-end
-
-class Timer
-  def initialize(sec)
-    @sec = sec
-    reset
-  end
-
-  def start
-    @state = :start
-    @start_time = Time.now
-    p @state
-  end
-
-  def pause
-    @state = :pause
-    @elapse = Time.now - @start_time + @elapse
-    p @state, @elapse
-  end
-
-  def stop
-  end
-
-  def reset
-    @state = :stop
-    @elapse = 0
-  end
-
-  def remain
-    if @state == :stop
-      @sec
-    elsif @state == :start
-      @start_time + @sec - @elapse - Time.now
-    elsif @state == :pause
-      @sec - @elapse
-    end
-  end
-
-  def toggle
-    if @state == :stop
-      start
-    elsif @state == :start
-      pause
-    elsif @state == :pause
-      start
-    end
-  end
-end
+require 'util'
 
 class TimerWindow
   def initialize(width, height)
@@ -80,7 +25,7 @@ class TimerWindow
   end
 
   def init_timer
-    @timer = Timer.new(100)
+    @timer = Timer.new(10)
     @time = Time.now
     Gtk::timeout_add(10) do
       if Time.now - @time > 0.1
--- util.rb
+++ util.rb
@@ -8,3 +8,50 @@ class Color
     @@color_table[key]
   end
 end
+
+class Timer
+  def initialize(sec)
+    @sec = sec
+    reset
+  end
+
+  def start
+    @state = :start
+    @start_time = Time.now
+    p @state
+  end
+
+  def pause
+    @state = :pause
+    @elapse = Time.now - @start_time + @elapse
+    p @state, @elapse
+  end
+
+  def stop
+  end
+
+  def reset
+    @state = :stop
+    @elapse = 0
+  end
+
+  def remain
+    if @state == :stop
+      @sec
+    elsif @state == :start
+      @start_time + @sec - @elapse - Time.now
+    elsif @state == :pause
+      @sec - @elapse
+    end
+  end
+
+  def toggle
+    if @state == :stop
+      start
+    elsif @state == :start
+      pause
+    elsif @state == :pause
+      start
+    end
+  end
+end
