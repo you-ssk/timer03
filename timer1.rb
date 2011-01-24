03f7194dcb809e54d030703166e64f1a6b47e911
you-ssk <inukichi.i@gmail.com>
2011-01-24 15:24:22 +0900

    test for display image, scale text to fit screen.

--- timer1.rb
+++ timer1.rb
@@ -1,43 +1,94 @@
-#require 'gnomecanvas2'
 require 'gtk2'
 require 'util'
 
+module View
+  def draw_bg(drawable)
+    gc = Gdk::GC.new(drawable)
+    width,height = drawable.size
+    gc.set_rgb_fg_color(Color["cadetblue"])
+    drawable.draw_rectangle(gc,true,0,0,width,height)
+    gc.set_rgb_fg_color(Color["#00FF00"])
+    drawable.draw_line(gc,0,0,width,height)
+    gc.set_rgb_fg_color(Color["#FF0000"])
+    drawable.draw_line(gc,width,0,0,height)
+  end
+
+  def draw_bg2(drawable)
+    gc = Gdk::GC.new(drawable)
+    width,height = drawable.size
+    gc.set_rgb_fg_color(Color["white"])
+    drawable.draw_rectangle(gc,true,0,0,width,height)
+    gc.set_rgb_fg_color(Color["red"])
+    step = 1.0*width/12
+    stripe = (0..width).step(step*2)
+    stripe.each do |s|
+      drawable.draw_rectangle(gc,true,s,0,step,height)
+    end
+  end
+
+  def draw_image(drawable,filename)
+    width,height = drawable.size
+    pixbuf = Images.scale(filename,width/2,height/2)
+    gc = Gdk::GC.new(drawable)
+    drawable.draw_pixbuf(gc,pixbuf,0,0,width/4,height/4,-1,-1,
+                         Gdk::RGB::DITHER_NORMAL, 0, 0)
+  end
+
+  def draw_text(drawable,remain_text)
+    gc = Gdk::GC.new(drawable)
+    width,height = drawable.size
+    font = Pango::FontDescription.new("Ubuntu")
+    font.absolute_size = height/2*Pango::SCALE
+    context = Gdk::Pango.context
+    context.font_description = font
+    layout = Pango::Layout.new(context)
+    layout.width = width*Pango::SCALE
+    layout.set_alignment(Pango::Layout::ALIGN_CENTER)
+    layout.text = remain_text
+    extents = layout.pixel_extents
+    drawable.draw_layout(gc, 0, height/2-(extents[1].height/2), layout, Color["darkorange"])
+  end
+end
+
 class TimerWindow
+  include View
   def initialize(width, height)
     init_window(width, height)
-    init_gc
     init_timer
+    init_images
+    @window.show_all
   end
 
   def init_window(width, height, window_type=nil)
+    @pixmap = nil
     window_type ||= Gtk::Window::TOPLEVEL
     @window = Gtk::Window.new(window_type)
     @window.set_default_size(width,height)
     @window.set_app_paintable(true)
     @window.realize
     set_window_signal(@window)
-    @window.show_all
-  end
-
-  def init_gc
-    @drawable = @window.window
-    @gc = Gdk::GC.new(@drawable)
+    p @window.window
   end
 
   def init_timer
     @timer = Timer.new(10)
-    @time = Time.now
-    Gtk::timeout_add(10) do
-      if Time.now - @time > 0.1
-        draw_pixmap(@window.window)
-        @time = Time.now
-      end
+    Gtk::timeout_add(100) do
+      draw_pixmap(@window.window)
       true
     end
   end
 
+  def init_images
+    Images['picture.JPG']
+  end
+
+
   def toggle_timer
     @timer.toggle
+  end
+
+  def reset_timer
+    @timer.reset
   end
 
   def set_window_signal(window)
@@ -53,8 +104,9 @@ class TimerWindow
     window.signal_connect("key_press_event") do |win,evt|
       case evt.keyval
       when Gdk::Keyval::GDK_Return
-        p "ret"
         toggle_timer
+      when Gdk::Keyval::GDK_space
+        reset_timer
       when Gdk::Keyval::GDK_Escape
         win.unfullscreen
       when Gdk::Keyval::GDK_F11
@@ -63,38 +115,23 @@ class TimerWindow
     end
   end
 
-  def draw(drawable)
-    gc = Gdk::GC.new(drawable)
-    width,height = drawable.size
-    gc.set_rgb_fg_color(Color["blue"])
-    drawable.draw_rectangle(gc,true,0,0,width,height)
-    gc.set_rgb_fg_color(Color["#00FF00"])
-    drawable.draw_line(gc,0,0,width,height)
-    gc.set_rgb_fg_color(Color["#FF0000"])
-    drawable.draw_line(gc,width,0,0,height)
-  end
-
-  def draw_text(drawable)
-    gc = Gdk::GC.new(drawable)
-    width,height = drawable.size
-    font = Pango::FontDescription.new("Ubuntu 40")
-    context = Gdk::Pango.context
-    context.font_description = font
-    layout = Pango::Layout.new(context)
-    layout.width = width*Pango::SCALE
-    layout.set_alignment(Pango::Layout::ALIGN_CENTER)
-#    layout.text = Time.now.strftime("%H:%M:%S:") + sprintf("%03d",Time.now.usec/1000)
-    layout.text = sprintf("%05.1f",@timer.remain)
-    drawable.draw_layout(gc, 0, height/2, layout, Color["white"])
-  end
-
   def draw_pixmap(window)
-    width, height = window.size
-    pixmap = Gdk::Pixmap.new(window, width, height, -1)
+    pixmap = get_pixmap(window)
+    draw_bg2(pixmap)
+    draw_image(pixmap,'picture.JPG')
+    draw_text(pixmap,@timer.remain_text)
     gc = Gdk::GC.new(window)
-    draw(pixmap)
-    draw_text(pixmap)
+    width, height = window.size
     window.draw_drawable(gc,pixmap,0,0,0,0,width,height)
+  end
+
+  def get_pixmap(window)
+    if !@pixmap || @pixmap.size != window.size
+      width, height = window.size
+      @pixmap = Gdk::Pixmap.new(window, width, height, -1)
+    else
+      @pixmap
+    end
   end
 end
 
--- util.rb
+++ util.rb
@@ -9,49 +9,94 @@ class Color
   end
 end
 
+class Images
+  @@image_table = Hash.new do |h,k|
+    h[k] = {
+      :org=>Gdk::Pixbuf.new(k),
+      :scaled=>nil
+    }
+  end
+  def self.[](key)
+    img = @@image_table[key]
+    if img[:scaled]
+      img[:scaled]
+    else
+      img[:org]
+    end
+  end
+
+  def self.scale(key,dest_width,dest_height)
+    if self[key].width != dest_width
+      org_image = @@image_table[key][:org]
+      scale = 1.0*dest_width/org_image.width
+      @@image_table[key][:scaled] = 
+        org_image.scale(scale*org_image.width,scale*org_image.height)
+    end
+    self[key]
+  end
+end
+
 class Timer
   def initialize(sec)
     @sec = sec
     reset
   end
 
-  def start
+  def start(time)
     @state = :start
-    @start_time = Time.now
+    @start_time = time
     p @state
   end
 
-  def pause
+  def pause(time)
     @state = :pause
-    @elapse = Time.now - @start_time + @elapse
-    p @state, @elapse
+    @elapse = time - @start_time + @elapse
+    p @state
   end
 
-  def stop
+  def expire
+    @state = :expire
+    p @state
   end
 
   def reset
     @state = :stop
     @elapse = 0
+    p @state
   end
 
   def remain
-    if @state == :stop
+    case @state
+    when :stop
       @sec
-    elsif @state == :start
-      @start_time + @sec - @elapse - Time.now
-    elsif @state == :pause
+    when :start
+      r = @start_time + @sec - @elapse - Time.now
+      if r > 0
+        r
+      else
+        expire
+        remain
+      end
+    when :pause
       @sec - @elapse
+    when :expire
+      0
     end
   end
 
+  def remain_text
+    m,s = remain.to_i.divmod(60)
+    sprintf("%02d:%02d",m,s)
+  end
+
   def toggle
-    if @state == :stop
-      start
-    elsif @state == :start
-      pause
-    elsif @state == :pause
-      start
+    case @state
+    when :stop
+      start(Time.now)
+    when :start
+      pause(Time.now)
+    when :pause
+      start(Time.now)
     end
   end
 end
